6bf99d14dee5e09874d4b3f32a0ed3b819f3c931
sandy21213 <83640165+sandy21213@users.noreply.github.com>
2021-11-12 14:40:51 +0530

    Add files via upload

---
+++ blockchain/index.js
@@ -0,0 +1,45 @@
+const Block = require('./block');
+
+class Blockchain {
+	constructor() {
+		this.chain = [Block.genesis()];
+	}
+
+	addBlock({ block }) {
+		return new Promise((resolve, reject)=>{
+			Block.validateBlock({
+				lastBlock: this.chain[this.chain.length -1],
+				block
+			}).then(()=>{
+				this.chain.push(block)
+				
+				return resolve();
+			}).catch(reject);
+		})
+	}
+
+	replaceChain({ chain }) {
+		return new Promise(async (resolve, reject) => {
+			for(let i = 0; i< chain.length; i++){
+				const block = chain[i];
+				const lastBlockIndex = i - 1;
+				const lastBlock = lastBlockIndex >= 0 ? chain[i-1] : null;
+
+				try {
+					await Block.validateBlock({ lastBlock, block });
+				} catch (error){
+					return reject(error);
+				}
+
+				console.log(`*-- Validated block number: ${block.blockHeaders.number}`)
+			}
+
+			this.chain = chain;
+
+			return resolve();
+		})
+	}
+}
+
+module.exports = Blockchain;
+
